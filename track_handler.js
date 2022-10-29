30a17dfecf9a9d90904ea8df235d20833461e7b2
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2022-10-29 10:12:46 -0500

    fix bug in which router would attempt to get matching song even if no song was provided

--- track_handler.js
+++ track_handler.js
@@ -31,126 +31,129 @@ router.get("/:song_id", (req, res, next) => {
 router.all("/loadSong/", (req, res) => {
   const song = req.body.song_data;
 
-  if (!song)
+  if (!song) {
     res
-      .code(400)
+      .status(400)
       .json({ error: true, response: "match_song body parameter required!" });
-  ytMusic
-    .searchMusics(`${song.title}  ${song.artists[0].name}`)
-    .then((musics) => {
-      const match_song = musics[0];
+  } else {
+    ytMusic
+      .searchMusics(`${song.title}  ${song.artists[0].name}`)
+      .then((musics) => {
+        const match_song = musics[0];
 
-      if (
-        !match_song ||
-        song.duration_seconds !== match_song.duration.totalSeconds
-      ) {
-        const match_song = song;
-        const uploadToDB = {
-          url: API_URL + "/db/musicData/",
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=UTF-8",
-          },
-          data: {
-            isSong: true,
-            original_id: "null",
-            song_id: match_song.videoId,
-            title: match_song.title,
-            artists: match_song.artists,
-            album: match_song.album,
-            thumbnail: match_song.thumbnails[0].url,
-            duration: match_song.duration || "0:00",
-            isExplicit: match_song.isExplicit,
-          },
-        };
-        axios(uploadToDB)
-          .then((db_response) => {
-            writeFile(db_response.data.response, () => {
-              res.json(db_response.data);
-            });
-          })
-          .catch((err) => console.log(err.response.data));
-      } else {
-        const uploadToDB = {
-          url: API_URL + "/db/musicData/",
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=UTF-8",
-          },
-          data: {
-            isSong: true,
-            original_id: song.videoId || "",
-            song_id: match_song.youtubeId,
-            title: match_song.title,
-            artists: match_song.artists,
-            album: match_song.album,
-            thumbnail: match_song.thumbnailUrl,
-            duration: match_song.duration.label,
-            isExplicit: match_song.isExplicit,
-          },
-        };
-        axios(uploadToDB)
-          .then((db_response) => {
-            writeFile(db_response.data.response, () => {
-              res.json(db_response.data);
-            });
-          })
-          .catch((err) => console.log(err.response.data));
-      }
-    });
-
-  function writeFile(match_song, cb) {
-    const song_id = match_song.song_id;
-
-    var YT_DLP = () => {
-      exec(
-        YT_DLP_LOCATION,
-        [
-          "-f",
-          "ba",
-          "-x",
-          "-o",
-          __dirname + `/assets/tracks/${song_id}.mp3`,
-          "--audio-format",
-          "mp3",
-          song_id,
-        ],
-        (error, data) => {
-          if (error) {
-            const options = {
-              url: API_URL + "/db/musicData/" + match_song.song_id,
-              method: "DELETE",
-              headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json;charset=UTF-8",
-              },
-              data: {},
-            };
-            axios(options)
-              .then((response) => {
-                res.status(500).json({
-                  error: true,
-                  response:
-                    response.data || "Unknown error occured (changes reverted)",
-                });
-              })
-              .catch((err) => {
-                res.status(500).json({
-                  error: true,
-                  response:
-                    err.response.data ||
-                    "Unknown error occured (error reverting changes)",
-                });
+        if (
+          !match_song ||
+          song.duration_seconds !== match_song.duration.totalSeconds
+        ) {
+          const match_song = song;
+          const uploadToDB = {
+            url: API_URL + "/db/musicData/",
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json;charset=UTF-8",
+            },
+            data: {
+              isSong: true,
+              original_id: "null",
+              song_id: match_song.videoId,
+              title: match_song.title,
+              artists: match_song.artists,
+              album: match_song.album,
+              thumbnail: match_song.thumbnails[0].url,
+              duration: match_song.duration || "0:00",
+              isExplicit: match_song.isExplicit,
+            },
+          };
+          axios(uploadToDB)
+            .then((db_response) => {
+              writeFile(db_response.data.response, () => {
+                res.json(db_response.data);
               });
-          } else {
-            cb();
-          }
+            })
+            .catch((err) => console.log(err.response.data));
+        } else {
+          const uploadToDB = {
+            url: API_URL + "/db/musicData/",
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json;charset=UTF-8",
+            },
+            data: {
+              isSong: true,
+              original_id: song.videoId || "",
+              song_id: match_song.youtubeId,
+              title: match_song.title,
+              artists: match_song.artists,
+              album: match_song.album,
+              thumbnail: match_song.thumbnailUrl,
+              duration: match_song.duration.label,
+              isExplicit: match_song.isExplicit,
+            },
+          };
+          axios(uploadToDB)
+            .then((db_response) => {
+              writeFile(db_response.data.response, () => {
+                res.json(db_response.data);
+              });
+            })
+            .catch((err) => console.log(err.response.data));
         }
-      );
-    };
-    YT_DLP();
+      });
+
+    function writeFile(match_song, cb) {
+      const song_id = match_song.song_id;
+
+      var YT_DLP = () => {
+        exec(
+          YT_DLP_LOCATION,
+          [
+            "-f",
+            "ba",
+            "-x",
+            "-o",
+            __dirname + `/assets/tracks/${song_id}.mp3`,
+            "--audio-format",
+            "mp3",
+            song_id,
+          ],
+          (error, data) => {
+            if (error) {
+              const options = {
+                url: API_URL + "/db/musicData/" + match_song.song_id,
+                method: "DELETE",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json;charset=UTF-8",
+                },
+                data: {},
+              };
+              axios(options)
+                .then((response) => {
+                  res.status(500).json({
+                    error: true,
+                    response:
+                      response.data ||
+                      "Unknown error occured (changes reverted)",
+                  });
+                })
+                .catch((err) => {
+                  res.status(500).json({
+                    error: true,
+                    response:
+                      err.response.data ||
+                      "Unknown error occured (error reverting changes)",
+                  });
+                });
+            } else {
+              cb();
+            }
+          }
+        );
+      };
+      YT_DLP();
+    }
   }
 });
 
