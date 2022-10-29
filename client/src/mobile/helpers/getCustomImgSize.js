9612b9644e8f17c0343aabd48166bd61691a6a5b
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2022-10-29 10:11:13 -0500

    fix img url retriever bug which returned images with a bad aspect ratio

--- client/src/mobile/helpers/getCustomImgSize.js
+++ client/src/mobile/helpers/getCustomImgSize.js
@@ -1,6 +1,6 @@
 const getCustomImgSize = (url, sizeX, sizeY) => {
   const trimmed_url = url.substring(0, url.lastIndexOf("="));
-  const return_url = trimmed_url + `=w${sizeX}-h${sizeY}-l90-rj`;
+  const return_url = trimmed_url + `=w${sizeX}-h${sizeY}-p-l90-rj`;
   return return_url;
 };
 
